9ac9b516d2e0a8944a0d9726f5d5181f4df3c12a
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-12-18 01:15:52 +0600

    user registration successful

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import httpStatus from 'http-status';
 const app: Application = express();
 import cookieParser from 'cookie-parser';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import router from './app/route';
 
 app.use(cors());
 
@@ -11,6 +12,9 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// global route
+app.use('/api/v1', router);
 
 // use global error handler
 app.use(globalErrorHandler);
